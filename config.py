12fed1edcb55aa9b994828c1301766aa349407b2
Kaniha R <kaniha1702@gmail.com>
2026-09-10 09:15:03 +0530

    Delete config.py

--- config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'your_default_secret_key'
-    DEBUG = os.environ.get('FLASK_DEBUG') or True
-    TESTING = os.environ.get('FLASK_TESTING') or False
-    JSON_SORT_KEYS = False
-    # Add any other configuration variables as needed.
